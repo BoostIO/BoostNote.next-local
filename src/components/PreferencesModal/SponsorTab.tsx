c8d5df4e98f0a81883053b5ff5534a51ad6dd7e8
Komediruzecki <komediruzecki.2015@gmail.com>
2021-08-19 17:58:41 +0200

    Fix sponsor tab messages

--- src/components/PreferencesModal/SponsorTab.tsx
+++ src/components/PreferencesModal/SponsorTab.tsx
@@ -1,23 +1,57 @@
 import React from 'react'
 import { SectionHeader } from './styled'
-import Button from '../../shared/components/atoms/Button'
-import { openNew } from '../../lib/platform'
 import Image from '../atoms/Image'
+import { PrimaryLink } from './AboutTab'
+import styled from '../../shared/lib/styled'
 
 const SponsorTab = () => {
   return (
     <div>
-      <SectionHeader>Please support our open source activity</SectionHeader>
-      <p>
-        We need your support to maintain Boost Note app sustainably. Thanks for
-        helping us!
-      </p>
+      <SectionHeader>
+        Please support the development of the free and open-source Boost Note
+        application.
+      </SectionHeader>
+      <SponsorText>
+        We need your support to maintain the Boost Note app sustainably. Thanks
+        for helping us!
+      </SponsorText>
       <div>
-        <Button
-          onClick={() => openNew('https://www.buymeacoffee.com/komediruzecki ')}
-        >
-          Komediruzecki - buy me a coffee
-        </Button>
+        <SponsorText>
+          The BoostIO company is currently focusing on developing the Boost Note
+          next cloud spaces. You can find more information about the cloud space
+          and its features on{' '}
+          <PrimaryLink href='https://boostnote.io/'>
+            Boost Note webpage
+          </PrimaryLink>
+          .
+        </SponsorText>
+
+        <SponsorText>
+          Boost Note local spaces are very popular with many satisfied users, so
+          the application will be further developed by the open-source community
+          and a designated maintainer{' '}
+          <PrimaryLink href='https://github.com/Komediruzecki'>
+            Komediruzecki
+          </PrimaryLink>
+          .
+        </SponsorText>
+        <SponsorText>
+          If you want to help support the development, you can do so by engaging
+          with our community, creating issues, and contributing to solving them,
+          as well as suggesting new features on our new{' '}
+          <PrimaryLink href='https://github.com/BoostIO/BoostNote.next-local'>
+            Github repository
+          </PrimaryLink>
+          .
+        </SponsorText>
+
+        <SponsorText>
+          If you want to support the maintainer please do so by{' '}
+          <PrimaryLink href={'https://www.buymeacoffee.com/komediruzecki'}>
+            following this link
+          </PrimaryLink>
+          .
+        </SponsorText>
       </div>
       <div>
         <Image
@@ -28,5 +62,10 @@ const SponsorTab = () => {
     </div>
   )
 }
+
+const SponsorText = styled.p`
+  max-width: 700px;
+  font-size: ${({ theme }) => theme.sizes.fonts.md}px;
+`
 
 export default SponsorTab
