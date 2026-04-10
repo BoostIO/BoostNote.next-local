29643d4d4ce34a0dd43f7e6354b7fb23435c7124
komediruzecki <komediruzecki.2015@gmail.com>
2026-04-10 19:46:56 +0200

    Update node and other deps
    Fix some webpack errors

--- webpack.config.ts
+++ webpack.config.ts
@@ -1,12 +1,13 @@
 import path from 'path'
 import webpack from 'webpack'
 import HtmlWebpackPlugin from 'html-webpack-plugin'
-import express from 'express'
-import ErrorOverlayPlugin from 'error-overlay-webpack-plugin'
 import CopyPlugin from 'copy-webpack-plugin'
 import packageJson from './package.json'
+import 'webpack-dev-server'
+// Make sure it's up top with your other imports like 'path' and 'webpack'
+import { Configuration } from 'webpack'
 
-module.exports = (env, argv) => {
+module.exports = (_: any, argv: Configuration) => {
   const config: webpack.Configuration = {
     entry: ['./src/index.tsx'],
 
@@ -47,15 +48,15 @@ module.exports = (env, argv) => {
     },
 
     plugins: [
-      new webpack.NamedModulesPlugin(),
+      // default
+      // new webpack.NamedModulesPlugin(),
       // prints more readable module names in the browser console on HMR updates
 
-      new webpack.NoEmitOnErrorsPlugin(),
+      // new webpack.NoEmitOnErrorsPlugin(),
       // do not emit compiled assets that include errors
       new HtmlWebpackPlugin({
         template: 'index.html',
       }),
-      new ErrorOverlayPlugin(),
       new webpack.DefinePlugin({
         'process.env.VERSION': JSON.stringify(packageJson.version),
       }),
@@ -63,7 +64,10 @@ module.exports = (env, argv) => {
       new CopyPlugin({
         patterns: [
           {
-            from: path.join(__dirname, 'node_modules/codemirror/lib/codemirror.css'),
+            from: path.join(
+              __dirname,
+              'node_modules/codemirror/lib/codemirror.css'
+            ),
             to: 'app/codemirror/theme/codemirror.css',
           },
           {
@@ -92,61 +96,164 @@ module.exports = (env, argv) => {
         ],
       }),
     ],
-
     devServer: {
       host: 'localhost',
       port: 3000,
+      client: {
+        overlay: true,
+      },
+      static: [
+        {
+          directory: path.join(__dirname, 'static'),
+          publicPath: '/app/static',
+        },
+        {
+          directory: path.join(__dirname, 'node_modules/codemirror/mode'),
+          publicPath: '/app/codemirror/mode',
+        },
+        {
+          directory: path.join(__dirname, 'node_modules/codemirror/addon'),
+          publicPath: '/app/codemirror/addon',
+        },
+        {
+          directory: path.join(__dirname, 'node_modules/codemirror/theme'),
+          publicPath: '/app/codemirror/theme',
+        },
+        {
+          directory: path.join(__dirname, 'node_modules/katex/dist'),
+          publicPath: '/app/katex',
+        },
+        {
+          directory: path.join(
+            __dirname,
+            'node_modules/remark-admonitions/styles'
+          ),
+          publicPath: '/app/remark-admonitions',
+        },
+        { directory: path.join(__dirname, 'public') }, // or __dirname if needed
+      ],
 
       historyApiFallback: {
         index: '/app',
       },
-      // respond to 404s with index.html
 
       hot: true,
-      // enable HMR on the server
 
-      before: function (app, server) {
-        app.use(
-          '/app/codemirror/mode',
-          express.static(path.join(__dirname, 'node_modules/codemirror/mode'))
-        )
-        app.use(
-          '/app/codemirror/addon',
-          express.static(path.join(__dirname, 'node_modules/codemirror/addon'))
-        )
-        app.use(
-          '/app/codemirror/theme',
-          express.static(path.join(__dirname, 'node_modules/codemirror/theme'))
-        )
-        app.use(
-          '/app/katex/katex.min.css',
-          express.static(
-            path.join(__dirname, 'node_modules/katex/dist/katex.min.css')
-          )
-        )
-        app.use(
-          '/app/remark-admonitions/classic.css',
-          express.static(
-            path.join(
-              __dirname,
-              'node_modules/remark-admonitions/styles/classic.css'
-            )
-          )
-        )
-        app.use('/app/static', express.static(path.join(__dirname, 'static')))
-      },
+      //   setupMiddlewares: (middlewares, app) => {
+      //     if (!app) {
+      //       throw new Error('webpack-dev-server is not defined')
+      //     }
+      //
+      //     middlewares.push({
+      //       name: '/app/codemirror/mode',
+      //       path: express.static(
+      //         path.join(__dirname, 'node_modules/codemirror/mode')
+      //       ),
+      //     })
+      //
+      //     app.server.use(
+      //       '/app/codemirror/addon',
+      //       express.static(path.join(__dirname, 'node_modules/codemirror/addon'))
+      //     )
+      //
+      //     app.server.use(
+      //       '/app/codemirror/theme',
+      //       express.static(path.join(__dirname, 'node_modules/codemirror/theme'))
+      //     )
+      //
+      //     app.server.use(
+      //       '/app/katex/katex.min.css',
+      //       express.static(
+      //         path.join(__dirname, 'node_modules/katex/dist/katex.min.css')
+      //       )
+      //     )
+      //
+      //     app.server.use(
+      //       '/app/remark-admonitions/classic.css',
+      //       express.static(
+      //         path.join(
+      //           __dirname,
+      //           'node_modules/remark-admonitions/styles/classic.css'
+      //         )
+      //       )
+      //     )
+      //
+      //     app.server.use(
+      //       '/app/static',
+      //       express.static(path.join(__dirname, 'static'))
+      //     )
+      //
+      //     return middlewares
+      //   },
+      // },
+      // devServer: {
+      //   host: 'localhost',
+      //   port: 3000,
+      //
+      //   historyApiFallback: {
+      //     index: '/app',
+      //   },
+      //   // respond to 404s with index.html
+      //
+      //   hot: true,
+      //   // enable HMR on the server
+      //   // @ts-ignore (Adding this so TypeScript doesn't yell at you if versions are mismatched)
+      //   setupMiddlewares: (middlewares, devServer) => {
+      //     if (devServer === undefined) {
+      //       throw new Error('webpack-dev-server is not defined')
+      //     }
+      //     // We cast to 'any' to bypass the "Property use does not exist" error
+      //     const app = devServer.server
+      //     app.server.use(
+      //       '/app/codemirror/mode',
+      //       express.static(path.join(__dirname, 'node_modules/codemirror/mode'))
+      //     )
+      //     app.server.use(
+      //       '/app/codemirror/addon',
+      //       express.static(path.join(__dirname, 'node_modules/codemirror/addon'))
+      //     )
+      //     app.server.use(
+      //       '/app/codemirror/theme',
+      //       express.static(path.join(__dirname, 'node_modules/codemirror/theme'))
+      //     )
+      //     app.server.use(
+      //       '/app/katex/katex.min.css',
+      //       express.static(
+      //         path.join(__dirname, 'node_modules/katex/dist/katex.min.css')
+      //       )
+      //     )
+      //     app.server.use(
+      //       '/app/remark-admonitions/classic.css',
+      //       express.static(
+      //         path.join(
+      //           __dirname,
+      //           'node_modules/remark-admonitions/styles/classic.css'
+      //         )
+      //       )
+      //     )
+      //     app.server.use('/app/static', express.static(path.join(__dirname, 'static')))
+      //
+      //     // You MUST return the middlewares array in this new syntax
+      //     return middlewares
+      //   },
     },
 
     resolve: {
+      fallback: {
+        path: require.resolve('path-browserify'),
+        crypto: require.resolve('crypto-browserify'),
+        stream: require.resolve('stream-browserify'),
+        fs: false,
+        buffer: require.resolve('buffer/'),
+        vm: require.resolve('vm-browserify'),
+      },
       extensions: ['.tsx', '.ts', '.js'],
-    },
-    node: {
-      fs: 'empty',
     },
   }
 
   if (argv.mode === 'development') {
-    config.plugins.unshift(new webpack.HotModuleReplacementPlugin())
+    // auto applied in 'hot' mode
+    // config.plugins.unshift(new webpack.HotModuleReplacementPlugin())
 
     config.entry = [
       'react-hot-loader/patch',
